e449f18229e0f7b49ab9782c92ba35fd7cef5811
FredrickHer <008222338@coyote.csusb.edu>
2024-02-01 11:52:11 -0800

    create hello world

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+* {This program will allow you to display a text which says hello world}
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World Again!\n");
+    return 0;
+}
